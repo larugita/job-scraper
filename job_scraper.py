5a71cf0dea5e37ee6a7a4ab592f46894f79bf04f
Nassim <nassimwarnerr@gmail.com>
2026-07-31 19:45:45 -0400

    Add Python job filter with CSV export and gitignore

--- job_scraper.py
+++ job_scraper.py
@@ -1,9 +1,9 @@
 import requests
+import csv
 from bs4 import BeautifulSoup
 
 url = "https://realpython.github.io/fake-jobs/"
 response = requests.get(url)
-print(response.status_code)
 
 soup = BeautifulSoup(response.text, "html.parser")
 
@@ -15,10 +15,22 @@ for job in jobs:
     title = job.find("h2", class_="title")
     company = job.find("h3", class_ = "company")
     apply_link = job.find("a", string="Apply")
-    url = apply_link["href"]
+    link = apply_link["href"]
     job_dict = {
         "title": title.text.strip(),
         "company": company.text.strip(),
-        "link": url
+        "link": link
     }
     jobs_data.append(job_dict)
+
+python_jobs = []
+
+for job in jobs_data:
+    if "python" in job["title"].lower():
+        python_jobs.append(job)
+
+if python_jobs:
+    with open("python_jobs.csv", "w", newline="") as file:
+        writer = csv.DictWriter(file, fieldnames= python_jobs[0].keys())
+        writer.writeheader()
+        writer.writerows(python_jobs)
