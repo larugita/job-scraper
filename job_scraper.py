e59cf3d17b8e3430f96b3122fc7b294e6f56d632
Nassim <nassimwarnerr@gmail.com>
2026-08-09 17:47:13 -0400

    Add job posting data

--- job_scraper.py
+++ job_scraper.py
@@ -16,12 +16,14 @@ def scrape_jobs(soup):
         title = job.find("h2", class_="title")
         company = job.find("h3", class_ = "company")
         location = job.find("p", class_ = "location")
+        date = job.find("time")
         apply_link = job.find("a", string="Apply")
         link = apply_link["href"]
         job_dict = {
             "title": title.text.strip(),
             "company": company.text.strip(),
             "location": location.text.strip(),
+            "date": date.text.strip(),
             "link": link
         }
         jobs_data.append(job_dict)
