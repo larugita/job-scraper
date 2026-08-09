2ee99f9d008615deaa09382e868a6a33cfbbfded
Nassim <nassimwarnerr@gmail.com>
2026-08-09 17:59:24 -0400

    feat: add job filtering

--- job_scraper.py
+++ job_scraper.py
@@ -4,31 +4,34 @@ from bs4 import BeautifulSoup
 
 def fetch_page(url):
     response = requests.get(url)
-    soup = BeautifulSoup(response.text, "html.parser")
-    return soup
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, "html.parser")
+        return soup
+    return None
 
 soup = fetch_page("https://realpython.github.io/fake-jobs/")
 
 def scrape_jobs(soup):
-    jobs = soup.find_all("div", class_="card")
-    jobs_data = []
-    for job in jobs:
-        title = job.find("h2", class_="title")
-        company = job.find("h3", class_ = "company")
-        location = job.find("p", class_ = "location")
-        date = job.find("time")
-        apply_link = job.find("a", string="Apply")
-        link = apply_link["href"]
-        job_dict = {
-            "title": title.text.strip(),
-            "company": company.text.strip(),
-            "location": location.text.strip(),
-            "date": date.text.strip(),
-            "link": link
-        }
-        jobs_data.append(job_dict)
-
-    return jobs_data
+    if soup:
+        jobs = soup.find_all("div", class_="card")
+        jobs_data = []
+        for job in jobs:
+            title = job.find("h2", class_="title")
+            company = job.find("h3", class_ = "company")
+            location = job.find("p", class_ = "location")
+            date = job.find("time")
+            apply_link = job.find("a", string="Apply")
+            link = apply_link["href"]
+            job_dict = {
+                "title": title.text.strip(),
+                "company": company.text.strip(),
+                "location": location.text.strip(),
+                "date": date.text.strip(),
+                "link": link
+            }
+            jobs_data.append(job_dict)
+        return jobs_data
+    return None
 
 jobs_data = scrape_jobs(soup)
 
