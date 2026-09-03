ae5b6d9329e4e3b05bedc781df6d76a3baf0b122
Nassim <nassimwarnerr@gmail.com>
2026-09-03 15:11:25 -0400

    Add date filtering by newest job

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def home():
     return {"message": "Job Scraper API is running"}
 
 @app.get("/jobs")
-def get_jobs(keyword: str = None, location: str = None, limit: int = 10):
+def get_jobs(keyword: str = None, location: str = None, limit: int = 10, sort: str = None):
     conn = sqlite3.connect("jobs.db")
     conn.row_factory = sqlite3.Row
     query = "SELECT * FROM jobs"
@@ -24,6 +24,10 @@ def get_jobs(keyword: str = None, location: str = None, limit: int = 10):
         else:
             query += " WHERE location like ?"
         params.append(f"%{location}%")
+
+    if sort == "newest":
+        query += " ORDER BY date DESC"
+    
     query += " LIMIT ?"
     params.append(limit)
 
